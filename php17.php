d8d70d0868513365d0d329a3675920a7bec13527
Sho Imai <aplieomavacere@gmail.com>
2018-07-12 17:37:18 +0900

    例外処理

---
+++ php17.php
@@ -0,0 +1,17 @@
+<?php
+
+//例外処理
+
+function div($a,$b){
+	try{
+	if ($b === 0){
+		throw new Exception("cannot divide by 0!");
+	}
+	echo $a/$b;
+	}catch(Exception $e){
+		echo $e->getMessage();
+	}
+}
+
+div(7,2);
+div(3,0);
