6bc613c74ded255639f74e20ad1a1ff4c9d74530
Sho Imai <aplieomavacere@gmail.com>
2018-07-12 15:03:29 +0900

    配列

---
+++ php10.php
@@ -0,0 +1,14 @@
+<?php
+
+// 配列
+$sales = [
+	"taguchi" => 200,
+	"imai" => 300,
+	"rin" =>400,
+];
+
+var_dump($sales["imai"]);
+var_dump($sales["rin"]);
+
+$colors = ["blue","red","green",];
+var_dump($colors[1]);
