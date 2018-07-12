3455385cc61950385c61a3b48899a3c3c6eb4053
araiyuki <vsn_2230801@i.softbank.jp>
2018-07-12 10:17:58 +0900

    hello world

---
+++ php01.php
@@ -0,0 +1,23 @@
+<?php
+
+// コメント
+#  コメント
+/*
+コメント
+コメント
+コメント
+*/
+echo "hello from the TOP!";
+?>
+
+
+
+
+<!DOCKTYPE html>
+<html lang="ja">
+<body>
+	<p>Hello World <?php echo"from PHP"; ?></p>
+
+</body>
+
+
