300cae139731ed45b115a70f10e82dd1b53921e4
araiyuki <vsn_2230801@i.softbank.jp>
2018-07-12 10:19:26 +0900

    変数/変数の型と値の見方(var_dump)

---
+++ php02.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+変数： データに付けるラベル
+
+データ型：
+- 文字列 string
+- 数値 integer float
+- 論理値 boolean/true false
+- 配列
+- オブジェクト
+- null
+
+*/
+
+$msg="Hello World" 
+echo $msg;
+
+var_dump($msg);
+
